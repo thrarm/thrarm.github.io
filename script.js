bc92b1b07548f052d08eaaf7c669c901fa2ff4f9
thrarm <47269995+thrarm@users.noreply.github.com>
2019-02-02 01:37:30 -0800

    Add files via upload
    batch upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.js--section-features')
+
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   })
+      
+});
